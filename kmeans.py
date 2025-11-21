b35d3b3ed4d166dca3d4231945acc725b2cd0cfa
wei485 <wei485@purdue.edu>
2025-11-21 03:00:55 +0000

    Final

---
+++ kmeans.py
@@ -0,0 +1,64 @@
+import numpy as np
+from cluster import createClusters
+from point import makePointList
+
+
+def kmeans(point_data, cluster_data):
+    """Performs k-means clustering on points.
+
+    Args:
+      point_data: a p-by-d numpy array used for creating a list of Points.
+      cluster_data: A k-by-d numpy array used for creating a list of Clusters.
+
+    Returns:
+      A list of clusters (with update centers) after peforming k-means
+      clustering on the points initialized from point_data
+    """
+    # Fill in
+
+    # 1. Make list of points using makePointList and point_data
+
+    # 2. Make list of clusters using createClusters and cluster_data
+
+    # 3. For as long as points keep moving between clusters:
+
+    #   A. Move every point to its closest cluster (use Point.closest and
+    #     Point.moveToCluster)
+    #     Hint: keep track here whether any point changed clusters by
+    #           seeing if any moveToCluster call returns "True"
+
+    #   B. Update the centers for each cluster (use Cluster.updateCenter)
+
+    # 4. Return the list of clusters, with the centers in their final positions
+    
+    points= makePointList(point_data)
+    clusters = createClusters(cluster_data)
+    
+    change=True
+    while change:
+      change = False
+      for p in points:
+        closest_cluster = p.closest(clusters)
+        if p.moveToCluster(closest_cluster):
+          change = True
+      
+      for c in clusters:
+        c.updateCenter()
+    
+    
+    return clusters
+
+
+if __name__ == "__main__":
+    data = np.array(
+        [
+            [12.1, -7.1], [0.5, 2.8], [1.2, 5.3], [10.3, -4.8], [-1.1, 3.9],
+            [8.9, -3.6], [11.5, -6.2], [7.4, -2.5], [10.8, -5.5], [9.4, -4.3]
+        ],
+        dtype=float,
+    )
+    centers = np.array([[0, 0], [1, 1]], dtype=float)
+
+    clusters = kmeans(data, centers)
+    for c in clusters:
+        c.printAllPoints()
